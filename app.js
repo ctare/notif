201adc4e1fed6e1c964a3ec372839667c3a773f9
ctare <sankaku900622@yahoo.co.jp>
2017-09-12 22:45:54 +0900

    delete

--- app.js
+++ app.js
@@ -1,50 +1,21 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
 var http = require('http');
 var app = express();
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-//===========================
-//ルーティング
 app.get('/', function(req, res) {
     console.log("ok");
-    // var commits = req.param('commits');
-    // var commit_message = "";
-    // for(var i=0; i<commits.length; i++) {
-    //     //マージのcommitメッセージはなくす
-    //     if(commits[i]['message'].indexOf('Merge branch') < 0 ) {
-    //         console.log(commits[i]['message']);
-    //         commit_message += commits[i]['message'];
-    //     }
-    // }
     io.sockets.emit('push', {value: "aaaa"});
     res.render('index', { title: 'Express' });
 });
-//===========================
 
-
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.set('port', process.env.PORT || 5000);
 
-// error handlers
-
-// development error handler
-// will print stacktrace
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
@@ -55,31 +26,9 @@ if (app.get('env') === 'development') {
     });
 }
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
 
-
-//Websokcet接続の部分
 var server = http.createServer(app);
-server.listen(app.get('port'), function() {
-    console.log("Express server listening on port " + app.get('port'));
-});
+server.listen(app.get('port'));
 var io = require('socket.io').listen(server);
-io.sockets.on('connection', function(socket) {
-    console.log('connection start');
-});
 
 module.exports = app;
